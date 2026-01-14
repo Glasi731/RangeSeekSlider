786ddb0c1d7bcf9072847d57825d790ba2e0c7de
Thomas Glas <63039728+Glasi731@users.noreply.github.com>
2026-01-14 10:34:46 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "RangeSeekSlider"
-)
+    name: "RangeSeekSlider",
+    products: [
+        .library(
+            name: "RangeSeekSlider",
+            targets: ["RangeSeekSlider"]),
+    ],
+    dependencies: [],
+    targets:[
+        .target(
+            name: "RangeSeekSlider",
+            path: "Sources"
+        )
+    ]
